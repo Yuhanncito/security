e7f079c41bc52db2bca2d413d2d28c41029a7d02
Yuhanncito <127004490+Yuhanncito@users.noreply.github.com>
2023-11-14 22:05:24 -0600

    Update aviso.jsx

--- src/views/aviso.jsx
+++ src/views/aviso.jsx
@@ -6,41 +6,161 @@ const AvisoPrivacidad = () => {
     };
 
     return (
-                <div style={{marginLeft: '5%', marginRight: '5%'}}>
-        <div style={{fontSize: '1.2rem', fontWeight: 'bold', textAlign: 'center', margin: '2rem'}}>
-            <p>Aviso de privacidad</p>
-        </div>
-        <div style={{fontSize: '1.1rem', marginBottom: '1rem'}}>
-            <p>Administradora de Comercios y Franquicias, con domicilio en calle Rayón Oriente No. 303-8 Plaza Robert, colonia Centro, ciudad México, municipio o delegación Tulancingo de Bravo, c.p. 43600, en la entidad de Hidalgo, país México, es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</p>
-        </div>
-        <div style={{fontSize: '1.1rem', marginBottom: '1rem'}}>
-            <p>¿Para qué fines utilizaremos sus datos personales?</p>
-            <p>Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</p>
+                <div>
+<div style={{margin: '2rem'}}>
+    <h1>AVISO DE PRIVACIDAD</h1>
+    <p>Administradora de Comercios y Franquicias , mejor conocido como Administradora de Comercios y Franquicias , con domicilio en calle Rayón Oriente No. 303-8 Plaza Robert, colonia Centro, ciudad México, municipio o delegación Tulancingo de Bravo, c.p. 43600, en la entidad de Hidalgo, país México , y portal de internet l.aguilar@gruporoma.com , es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:</p>
+    <h2>¿Para qué fines utilizaremos sus datos personales?</h2>
+    <p>Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</p>
+    <p>De manera adicional, utilizaremos su información personal para las siguientes finalidades secundarias que no son necesarias para el servicio solicitado, pero que nos permiten y facilitan brindarle una mejor atención:</p>
+    <ul>
+    <li>Cumplimiento de normativas y políticas internas</li>
+    <li>Cumplimiento de obligaciones legales</li>
+    <li>para verificar su identidad</li>
+    </ul>
+    <p>En caso de que no desee que sus datos personales se utilicen para estos fines secundarios, indíquelo a continuación:</p>
+    <p>No consiento que mis datos personales se utilicen para los siguientes fines:</p>
+    <ul>
+    <li>[  ] Cumplimiento de normativas y políticas internas</li>
+    <li>[  ] Cumplimiento de obligaciones legales</li>
+    <li>[  ] para verificar su identidad</li>
+    </ul>
+    <p>La negativa para el uso de sus datos personales para estas finalidades no podrá ser un motivo para que le neguemos los servicios y productos que solicita o contrata con nosotros.</p>
+    <h2>¿Qué datos personales utilizaremos para estos fines?</h2>
+    <p>Para llevar a cabo las finalidades descritas en el presente aviso de privacidad, utilizaremos los siguientes datos personales:</p>
+    <ul>
+    <li>Nombre</li>
+    <li>Estado Civil</li>
+    <li>Registro Federal de Contribuyentes(RFC)</li>
+    <li>Clave única de Registro de Población (CURP)</li>
+    <li>Lugar de nacimiento</li>
+    <li>Fecha de nacimiento</li>
+    <li>Nacionalidad</li>
+    <li>Domicilio</li>
+    <li>Teléfono particular</li>
+    <li>Teléfono celular</li>
+    <li>Correo electrónico</li>
+    <li>Firma electrónica</li>
+    <li>Edad</li>
+    <li>Fotografía</li>
+    <li>Datos de identificación</li>
+    <li>Datos de contacto</li>
+    <li>Datos sobre características físicas</li>
+    <li>Datos laborales</li>
+    <li>Datos académicos</li>
+    </ul>
+    <h2>¿Cómo puede acceder, rectificar o cancelar sus datos personales, u oponerse a su uso?</h2>
+    <p>Usted tiene derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea inexacta o incompleta (Rectificación); que la eliminemos de nuestros registros o bases de datos cuando considere que la misma no está siendo utilizada adecuadamente (Cancelación); así como oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como derechos ARCO.</p>
+    <p>Para el ejercicio de cualquiera de los derechos ARCO, usted deberá presentar la solicitud respectiva a través del siguiente medio:</p>
+    <p>Acceso, Rectificación, Cancelación</p>
+    <p>Para conocer el procedimiento y requisitos para el ejercicio de los derechos ARCO, ponemos a su disposición el siguiente medio:</p>
+    <p>Avisos de Privacidad, Sitio web, Atención al cliente</p>
+    <p>Los datos de contacto de la persona o departamento de datos personales, que está a cargo de dar trámite a las solicitudes de derechos ARCO, son los siguientes:</p>
+    <ul>
+    <li>a) Nombre de la persona o departamento de datos personales: Responsable de Privacidad</li>
+    <li>b) Domicilio: calle Rayón Oriente No. 303-8 Plaza Robert, colonia Centro, ciudad México, municipio o delegación Tulancingo de Bravo, c.p. 43600, en la entidad de Hidalgo, país México</li>
+    </ul>
+    <h2>Usted puede revocar su consentimiento para el uso de sus datos personales</h2>
+    <p>Usted puede revocar el consentimiento que, en su caso, nos haya otorgado para el tratamiento de sus datos personales. Sin embargo, es importante que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligación legal requiramos seguir tratando sus datos personales. Asimismo, usted deberá considerar que para ciertos fines, la revocación de su consentimiento implicará que no le podamos seguir prestando el servicio que nos solicitó, o la conclusión de su relación con nosotros.</p>
+    <p>Para revocar su consentimiento deberá presentar su solicitud a través del siguiente medio:</p>
+    <p>Avisos de privacidad</p>
+    <p>Para conocer el procedimiento y requisitos para la revocación del consentimiento, ponemos a su disposición el siguiente medio:</p>
+    <p>Avisos de privacidad</p>
+    <h2>¿Cómo puede limitar el uso o divulgación de su información personal?</h2>
+    <p>Con objeto de que usted pueda limitar el uso y divulgación de su información personal, le ofrecemos los siguientes medios:</p>
+    <p>Configuración de notificaciones y preferencias, avisos de privacidad</p>
+    <p>Asimismo, usted se podrá inscribir a los siguientes registros, en caso de que no desee obtener publicidad de nuestra parte:</p>
+    <p>Registro Público para Evitar Publicidad, para mayor información consulte el portal de internet de la PROFECO</p>
+    <h2>El uso de tecnologías de rastreo en nuestro portal de internet</h2>
+    <p>Le informamos que en nuestra página de internet utilizamos cookies, web beacons u otras tecnologías, a través de las cuales es posible monitorear su comportamiento como usuario de internet, así como brindarle un mejor servicio y experiencia al navegar en nuestra página. Los datos personales que recabamos a través de estas tecnologías, los utilizaremos para los siguientes fines:</p>
+    <p>Seguridad</p>
+    <p>Los datos personales que obtenemos de estas tecnologías de rastreo son los siguientes:</p>
+    <ul>
+    <li>Identificadores, nombre de usuario y contraseñas de una sesión</li>
+    <li>Tipo de navegador del usuario</li>
+    <li>Tipo de sistema operativo del usuario</li>
+    <li>Páginas web visitadas por un usuario</li>
+    </ul>
+    <h2>¿Cómo puede conocer los cambios en este aviso de privacidad?</h2>
+    <p>El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras propias necesidades por los productos o servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios en nuestro modelo de negocio, o por otras causas.</p>
+    <p>Nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente aviso de privacidad, a través de: Avisos en el sitio web.</p>
+    <p>El procedimiento a través del cual se llevarán a cabo las notificaciones sobre cambios o actualizaciones al presente aviso de privacidad es el siguiente:</p>
+    <p>correo electrónico</p>
+    <h2>Su consentimiento para el tratamiento de sus datos personales</h2>
+    <p>Consiento que mis datos personales sean tratados de conformidad con los términos y condiciones informados en el presente aviso de privacidad.[  ]</p>
+    </div>
+        <div style={{fontSize: '1.1rem', marginBottom: '1rem', marginLeft: '5%', marginRight: '5%'}}>
+            <h2>Política de Cookies</h2>
+
+            <h3>Definiciones y términos clave</h3>
+            <p>Para ayudar a explicar las cosas de la manera más clara posible en esta Política de cookies, cada vez que se cumpla alguno de estos términos están referenciados, se definen estrictamente como:</p>
             <ul style={{fontSize: '1.1rem', listStyleType: 'disc', paddingLeft: '2rem'}}>
+                <li>Cookie: pequeña cantidad de datos generados por un sitio web y guardados por su navegador web. Es se utiliza para identificar su navegador, proporcionar análisis, recordar información sobre usted, como su preferencia de idioma o información de inicio de sesión.</li>
+                <li>Compañía: cuando esta política menciona "Compañía", "nosotros", " nos " o "nuestro", se refiere a, que es responsable de su información en virtud de esta Política de cookies. Dispositivo: cualquier dispositivo conectado a Internet, como un teléfono, tableta, computadora o cualquier otro dispositivo que se puede usar para visitar... and y utilizar los servicios.</li>
+                <li>Datos personales: cualquier información que directa, indirectamente o en relación con otros la información, incluido un número de identificación personal, permite la identificación o identificabilidad de una persona física.</li>
+                <li>Servicio: se refiere al servicio prestado por términos (si están disponibles) y en esta plataforma. as como se describe en el pariente</li>
+                <li>Servicio de terceros: se refiere al anunciante, patrocinadores de concursos, promocionales y de marketing socios y otros que proporcionan nuestro contenido o cuyos productos o servicios creemos que pueden te interesa.</li>
+                <li>Aplicación/Aplicación: app aplicación, se refiere al PRODUCTO DE SOFTWARE identificado arriba.</li>
+                <li>Usted: una persona o entidad registrada con para utilizar los Servicios.</li>
             </ul>
-        </div>
-        <div style={{fontSize: '1.1rem', marginBottom: '1rem'}}>
-            <p>De manera adicional, utilizaremos su información personal para las siguientes finalidades secundarias que no son necesarias para el servicio solicitado, pero que nos permiten y facilitan brindarle una mejor atención:</p>
+
+            <h3>Introducción</h3>
+            <p>Esta Política de Cookies explica cómo ... y sus afiliados (colectivamente "nosotros"," nos"y " nuestro"), utilizamos cookies y tecnologías similares para reconocerlo cuando visita nuestra aplicación, incluidos, entre otros, y cualquier URL relacionada, versión móvil o localizada y relacionada dominios/subdominios ("Sitios web"). Explica qué son estas tecnologías y por qué las utilizamos ellos, así como las opciones sobre cómo controlarlas.</p>
+
+            <h3>¿Qué es una cookie?</h3>
+            <p>Una cookie es un pequeño archivo de texto que se almacena en su computadora u otro dispositivo conectado a Internet para identificar su navegador, proporcionar análisis, recordar información sobre usted, como su preferencia de idioma o información de inicio de sesión. Son completamente seguros y no se pueden usar para correr programas o enviar virus a su dispositivo.</p>
+
+            <h3>¿Por qué utilizamos cookies?</h3>
+            <p>Utilizamos cookies propias y/o de terceros en nuestra aplicación para diversos fines. Tales como:</p>
             <ul style={{fontSize: '1.1rem', listStyleType: 'disc', paddingLeft: '2rem'}}>
-            <li>Cumplimiento de normativas y políticas internas</li>
-            <li>Cumplimiento de obligaciones legales</li>
-            <li>Para verificar su identidad</li>
+                <li>Para facilitar el funcionamiento y la funcionalidad de nuestra aplicación;</li>
+                <li>Para mejorar su experiencia con nuestra aplicación y hacer que navegar por ella sea más rápido y más fácil;</li>
+                <li>Para permitirnos crear una experiencia de usuario personalizada para usted y para que entendamos qué es útil o de su interés;</li>
+                <li>Para analizar cómo se usa nuestra aplicación y cuál es la mejor manera de personalizarla.;</li>
+                <li>Para identificar perspectivas de futuro y personalizar las interacciones de marketing y ventas con él.;</li>
+                <li>Para facilitar la adaptación de la publicidad en línea a sus intereses.</li>
             </ul>
-        </div>
-        <div style={{fontSize: '1.1rem', marginBottom: '1rem'}}>
-            <p>En caso de que no desee que sus datos personales se utilicen para estos fines secundarios, indíquelo a continuación:</p>
-            <p>No consiento que mis datos personales se utilicen para los siguientes fines:</p>
-            <ul style={{fontSize: '1.1rem', listStyleType: 'none', paddingLeft: '1rem'}}>
-            <li>[  ] Cumplimiento de normativas y políticas internas</li>
-            <li>[  ] Cumplimiento de obligaciones legales</li>
-            <li>[  ] Para verificar su identidad</li>
+
+            <h3>Qué tipo de cookies utiliza</h3>
+            <p>Las cookies pueden ser cookies de sesión o cookies persistentes. Una cookie de sesión caduca automáticamente al cerrar el navegador. Una cookie persistente permanecerá hasta que caduque o usted borre sus cookies. Las fechas de caducidad se establecen en las propias cookies; algunas pueden caducar después de un poco minuto, mientras que otros pueden caducar después de varios años. Cookies colocadas por el sitio web que está visitando las visitas se denominan "cookies de origen". Las cookies estrictamente necesarias son necesarias para que nuestra aplicación funcione y no se pueden desactivar en nuestros sistemas. Son esenciales para permitirle navegar por la aplicación y utilizar sus características. Si elimina o deshabilita estas cookies, no podemos garantizar que pueda usarlas nuestra aplicación.</p>
+            <p>Utilizamos los siguientes tipos de cookies en nuestra aplicación:</p>
+            <ul style={{fontSize: '1.1rem', listStyleType: 'disc', paddingLeft: '2rem'}}>
+                <li>Cookies Esenciales</li>
+                <li>Cookies de Rendimiento y Funcionalidad</li>
+                <li>Cookies de Marketing</li>
+                <li>Cookies de Análisis y Personalización</li>
+                <li>Cookies de Redes Sociales</li>
             </ul>
-            <p>La negativa para el uso de sus datos personales para estas finalidades no podrá ser un motivo para que le neguemos los servicios y productos que solicita o contrata con nosotros.</p>
-        </div>
-        <div style={{fontSize: '1.1rem', marginBottom: '1rem'}}>
-            <p>¿Dónde puedo consultar el aviso de privacidad integral?</p>
-            <p>Para conocer mayor información sobre los términos y condiciones en que serán tratados sus datos personales, como los terceros con quienes compartimos su información personal y la forma en que podrá ejercer sus derechos ARCO, puede consultar el aviso de privacidad integral en:</p>
-            <p>Comunicación directa, sitios web, puestos de atención al cliente</p>
+
+            <h3>¿Cómo puede administrar las cookies?</h3>
+            <p>La mayoría de los navegadores le permiten controlar las cookies a través de sus preferencias de 'configuración'. Sin embargo, si limite la capacidad de los sitios web para establecer cookies, puede empeorar su experiencia general de usuario, ya que ya no será personalizado para usted. También puede impedirle guardar configuraciones personalizadas como información de inicio de sesión. Los fabricantes de navegadores proporcionan páginas de ayuda relacionadas con la gestión de cookies en sus productos.</p>
+            <p>Los fabricantes de navegadores proporcionan páginas de ayuda relacionadas con la administración de cookies en sus productos. Consulte a continuación para obtener más información.</p>
+            <ul style={{fontSize: '1.1rem', listStyleType: 'disc', paddingLeft: '2rem'}}>
+                <li>Herramientas de Google Chrome</li>
+                <li>Navegador de Internet</li>
+                <li>Mozilla Firefox</li>
+                <li>Safari (Escritorio)</li>
+                <li>Safari (Móvil)</li>
+                <li>Navegador de Android</li>
+                <li>Ópera</li>
+                <li>Opera Móvil</li>
+            </ul>
+
+            <h3>Bloqueo y desactivación de cookies y similares tecnologías</h3>
+            <p>Dondequiera que se encuentre, también puede configurar su navegador para bloquear las cookies y similares tecnologías, ¡pero esta acción puede bloquear nuestras cookies esenciales y evitar que nuestra aplicación que funcione correctamente, y es posible que no pueda utilizar plenamente todas sus funciones y servicios. Tú también debe tener en cuenta que también puede perder cierta información guardada (por ejemplo, detalles de inicio de sesión guardados, sitio preferencias) si bloquea las cookies en su navegador. Diferentes navegadores hacen diferentes controles disponibles para usted. Deshabilitar una cookie o categoría de cookie no elimina la cookie de su navegador, deberá hacerlo usted mismo desde su navegador, debe visitar su menú de ayuda del navegador para más información.</p>
+
+            <h3>Cambios En Nuestra Política De Cookies</h3>
+            <p>Podemos cambiar nuestro Servicio y políticas, y es posible que debamos realizar cambios en esta Cookie. Política para que reflejen con precisión nuestro Servicio y políticas. A menos que la ley exija lo contrario, le notificaremos (por ejemplo, a través de nuestro Servicio) antes de realizar cambios en esta Cookie. Política y darle la oportunidad de revisarlas antes de que entren en vigencia. Entonces, si continúas para utilizar el Servicio, estará sujeto a la Política de cookies actualizada. Si no desea aceptar esta o cualquier Política de cookies actualizada, puede eliminar su cuenta.</p>
+
+            <h3>Su Consentimiento</h3>
+            <p>Al utilizar nuestra aplicación, registrar una cuenta o realizar una compra, usted acepta nuestra Política de cookies y acepto sus términos.</p>
+
+            <h3>Contacte con Nosotros</h3>
+            <p>No dude en ponerse en contacto con nosotros si tiene alguna pregunta sobre nuestras Cookies: Política.</p>
+            <ul style={{fontSize: '1.1rem', listStyleType: 'none', paddingLeft: '2rem'}}>
+                <li>Por Correo Electrónico: 20211045@uthh.edu.mx</li>
+            </ul>
+            <br /><br /><br /><br />
         </div>
         </div>
     );
